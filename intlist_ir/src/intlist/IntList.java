69176afd872a75b163c1326de758c1f8eea595e0
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-02-28 17:11:29 +0100

    Implementatie met gelinkte lijst

--- intlist_ir/src/intlist/IntList.java
+++ intlist_ir/src/intlist/IntList.java
@@ -1,6 +1,7 @@
 package intlist;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Each instance of this class stores a sequence of int values.
@@ -10,23 +11,66 @@ import java.util.Arrays;
  */
 public class IntList {
 	
+	private class Node {
+		private int value;
+		private Node next;
+	}
+	
+	/**
+	 * @invar | 0 <= size
+	 * @invar | IntStream.range(0, size + 1).allMatch(i -> (getNodes()[i] == null) == (i == size))
+	 */
+	private Node head;
+	private int size;
+	
+	/**
+	 * @representationObjects
+	 */
+	private Node[] getNodes() {
+		Node[] nodes = new Node[size + 1];
+		int i = 0;
+		Node n = head;
+		for (;;) {
+			nodes[i++] = n;
+			if (i == size + 1)
+				break;
+			n = n.next;
+		}
+		return nodes;
+	}
+	
 	/**
 	 * @creates | result
 	 */
-	public int[] getElements() { throw new RuntimeException("Not yet implemented"); }
+	public int[] getElements() {
+		int[] result = new int[size];
+		Node n = head;
+		for (int i = 0; i < size; i++) {
+			result[i] = n.value;
+			n = n.next;
+		}
+		return result;
+	}
 	
-	public int getLength() { throw new RuntimeException("Not yet implemented"); }
+	public int getLength() {
+		return size;
+	}
 	
 	/**
 	 * @pre | 0 <= index && index < getLength()
 	 * @post | result == getElements()[index]
 	 */
-	public int getElementAt(int index) { throw new RuntimeException("Not yet implemented"); }
+	public int getElementAt(int index) {
+		Node n = head;
+		for (int i = 0; i < index; i++)
+			n = n.next;
+		return n.value;
+	}
 
 	/**
 	 * @post | getLength() == 0
 	 */
-	public IntList() { throw new RuntimeException("Not yet implemented"); }
+	public IntList() {}
 	
 	/**
 	 * @mutates | this
@@ -34,12 +78,34 @@ public class IntList {
 	 * @post | Arrays.equals(getElements(), 0, old(getLength()), old(getElements()), 0, old(getLength()))
 	 * @post | getElements()[old(getLength())] == element
 	 */
-	public void add(int element) { throw new RuntimeException("Not yet implemented"); }
+	public void add(int element) {
+		if (head == null) {
+			head = new Node();
+			head.value = element;
+		} else {
+			Node n = head;
+			while (n.next != null)
+				n = n.next;
+			n.next = new Node();
+			n.next.value = element;
+		}
+		size++;
+	}
 	
 	/**
 	 * @pre | getLength() > 0
 	 * @mutates | this
 	 * @post | Arrays.equals(getElements(), 0, getLength(), old(getElements()), 0, getLength())
 	 */
-	public void removeLast() { throw new RuntimeException("Not yet implemented"); }
+	public void removeLast() {
+		if (head.next == null) {
+			head = null;
+		} else {
+			Node n = head;
+			while (n.next.next != null)
+				n = n.next;
+			n.next = null;
+		}
+		size--;
+	}
 }
